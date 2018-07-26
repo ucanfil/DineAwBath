24c6349ac76d1d98cc3eb7b1f31f72ffd849558e
Burak Tilek <burak.tilek@gmail.com>
2018-07-26 13:44:33 +0300

    State added, in componentDidMount places data is get from PlacesAPI

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react'
 import './App.css'
-import logo from './icons/funnel.svg'
-import MapContainer from './components/Map.js'
+import Sidebar from './components/Sidebar'
+import MapContainer from './components/Map'
+import * as PlacesAPI from './components/PlacesAPI'
 
 class App extends Component {
+  state = {
+    places: []
+  }
+
+  componentDidMount() {
+    PlacesAPI.getAll().then(places => {
+      this.setState( {places: places.response.venues} )
+    });
+  }
+
   toggleSidebar() {
 
   }
 
   render() {
+    console.log(this.state.places);
     return (
       <div className="App">
         <div className="container">
-          <aside className="sidebar">
-            <h1>DineOBath<br />Find Restaurants in Bath</h1>
-            <button id="hamburger-icon" onClick={this.toggleSidebar}>&#9776</button>
-            <div className="filtering">
-              <input id="search" type="text" placeholder="  e.g. Pizza, Breakfast, Wine..." aria-label="Search Restaurants" />
-              <button><img src={logo} alt="funnel" width="16px" height="16px" /> Filter</button>
-            </div>
-            <ul className="restaurant-list">
-              <li>Restaurant 1</li>
-              <li>Restaurant 2</li>
-              <li>Restaurant 3</li>
-            </ul>
-            <p>> This app uses foursquare places and google maps api</p>
-          </aside>
+          <Sidebar places={this.state.places}/>
           <MapContainer />
         </div>
       </div>
