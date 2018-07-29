b636ebe787cd27136496ae88828cc9bf0860bc86
Burak Tilek <burak.tilek@gmail.com>
2018-07-29 14:51:56 +0300

    Initial commit of component

---
+++ src/components/Modal.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+// Kent C. Dodds >> https://codesandbox.io/s/00254q4n6p
+
+const modalRoot = document.querySelector("#modal-root")
+
+class Modal extends Component {
+  render() {
+    return ReactDOM.createPortal(
+      <div
+        style={{
+          position: 'absolute',
+          top: '0',
+          bottom: '0',
+          left: '0',
+          right: '0',
+          display: 'grid',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'rgba(0,0,0,0.3)',
+          zIndex: 9999
+        }}
+        onClick={this.props.onClose}
+      >
+        <div
+          style={{
+            padding: 20,
+            background: '#fff',
+            borderRadius: '2px',
+            display: 'inline-block',
+            minHeight: '300px',
+            margin: '1rem',
+            position: 'relative',
+            minWidth: '300px',
+            boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+            justifySelf: 'center',
+            zIndex: 10000
+          }}
+        >
+          {this.props.children}
+          <hr />
+          <button onClick={this.props.onClose}>Close</button>
+        </div>
+      </div>,
+      modalRoot,
+    )
+  }
+}
+
+export default Modal
