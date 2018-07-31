42643d0e413607ceac2a34bbb51de7a0dbf2a4f3
Burak Tilek <burak.tilek@gmail.com>
2018-07-31 11:37:19 +0300

    Aria attributes added

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 class Category extends Component {
   render() {
     return (
-      <div className={this.props.title}>
-        <h2>{this.props.title}</h2>
+      <div className={this.props.title} aria-label="Cafe Categories">
+        <h2 id="categoryheader">{this.props.title}</h2>
         {this.props.children}
       </div>
     )
